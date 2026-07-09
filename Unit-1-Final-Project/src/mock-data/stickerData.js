730a42a40f72d3f290be0cc760e26d34100950b6
Riona <rionav02@gmail.com>
2026-07-09 09:35:19 -0500

    Added mock data file.

---
+++ Unit-1-Final-Project/src/mock-data/stickerData.js
@@ -0,0 +1,98 @@
+export const stickerData = [
+    {
+        id: 1,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/kiana1.png",
+        characters: "Kiana Kaslana"
+    },
+    {
+        id: 2,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/kiana2.png",
+        characters: "Kiana Kaslana"
+    },
+    {
+        id: 3,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/kiana3.png",
+        characters: "Kiana Kaslana"
+    },
+    {
+        id: 4,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/kiana4.png",
+        characters: "Kiana Kaslana"
+    },
+    {
+        id: 5,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/kiana5.png",
+        characters: "Kiana Kaslana"
+    },
+    {
+        id: 6,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/mei1.png",
+        characters: "Raiden Mei"
+    },
+    {
+        id: 7,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/mei2.png",
+        characters: "Raiden Mei"
+    },
+    {
+        id: 8,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/mei3.png",
+        characters: "Raiden Mei"
+    },
+    {
+        id: 9,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/mei4.png",
+        characters: "Raiden Mei"
+    },
+    {
+        id: 10,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/mei5.png",
+        characters: "Raiden Mei"
+    },
+    {
+        id: 11,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/bronya1.png",
+        characters: "Bronya Zaychik"
+    },
+    {
+        id: 12,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/bronya2.png",
+        characters: "Bronya Zaychik"
+    },
+    {
+        id: 13,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/bronya3.png",
+        characters: "Bronya Zaychik"
+    },
+    {
+        id: 14,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/bronya4.png",
+        characters: "Bronya Zaychik"
+    },
+    {
+        id: 15,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/bronya5.png",
+        characters: "Bronya Zaychik"
+    },
+    {
+        id: 16,
+        set: "From Finality, the Origin",
+        url: "./src/assets/stickers/From Finality the Origin/trio1.png",
+        characters: "Kiana Kaslana, Raiden Mei, Bronya Zaychik"
+    }
+];
