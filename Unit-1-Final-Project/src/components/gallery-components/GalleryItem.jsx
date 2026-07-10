b43be6357768eb1b53f6ce53fcf51e62ee09363c
Riona <rionav02@gmail.com>
2026-07-10 12:56:25 -0500

    Tweaks to how props are received so the props can be used more flexibly.

--- Unit-1-Final-Project/src/components/gallery-components/GalleryItem.jsx
+++ Unit-1-Final-Project/src/components/gallery-components/GalleryItem.jsx
@@ -1,16 +1,29 @@
 export default function GalleryItem(props) {
 
+    let stickerSource = props.src;
+    let stickerSet = props.set;
+    let stickerCharacters = props.characters;
+    let stickerInformation = `
+    Set: ${stickerSet}
+    Featured characters: ${stickerCharacters}
+    `;
+
     return (
 
         <div className="gallery-item">
             <img 
                 className="sticker"
-                src={props.src}
-                alt={props.characters}
-                title={props.characters}
+                src={stickerSource}
+                alt={stickerInformation}
+                title={stickerCharacters}
+                onClick={handleStickerClick}
             />
         </div>
 
     );
 
+}
+
+function handleStickerClick(event) {
+    console.log(event.target.alt);
 }
