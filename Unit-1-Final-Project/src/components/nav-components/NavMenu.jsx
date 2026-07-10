58f7a37ed5059656a25bb83b752c05a8709ce070
Riona <rionav02@gmail.com>
2026-07-10 16:12:33 -0500

    Lifted state from child components.

--- Unit-1-Final-Project/src/components/nav-components/NavMenu.jsx
+++ Unit-1-Final-Project/src/components/nav-components/NavMenu.jsx
@@ -1,13 +1,15 @@
 import NavDropdown from "./NavDropdown.jsx";
 import NavIcons from "./NavIcons.jsx";
 
-export default function NavMenu() {
+export default function NavMenu({galleryState, setGalleryState}) {
     
     return (
 
         <nav id="nav-section">
             
-            <NavDropdown/>
+            {/* PASS DOWN DROPDOWN SELECTION AND FUNCTION TO UPDATE IT AS PROPS */}
+            <NavDropdown galleryState={galleryState} setGalleryState={setGalleryState}/>
+            
             <NavIcons/>
 
         </nav>
