5100067833b344e63e00672c3c4d47f2836af7ff
Riona <rionav02@gmail.com>
2026-07-10 12:55:51 -0500

    Minor tweaks to props passed to children.

--- Unit-1-Final-Project/src/components/gallery-components/GalleryBody.jsx
+++ Unit-1-Final-Project/src/components/gallery-components/GalleryBody.jsx
@@ -13,6 +13,7 @@ export default function GalleryBody() {
                 <GalleryItem 
                 key={sticker.id} 
                 src={sticker.url} 
+                set = {sticker.set}
                 characters={sticker.characters}
                 />
                 
