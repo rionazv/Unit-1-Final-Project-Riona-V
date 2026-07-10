347e8a2837e0c5ed64e47292e77dd822184a1aec
Riona <rionav02@gmail.com>
2026-07-10 16:11:23 -0500

    Lifted state of some child components to parent component.

--- Unit-1-Final-Project/src/App.jsx
+++ Unit-1-Final-Project/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react'
 import Footer from './components/Footer'
 import GalleryBody from './components//gallery-components/GalleryBody'
 import Header from './components/Header.'
@@ -6,12 +7,20 @@ import NavMenu from './components/nav-components/NavMenu'
 
 function App() {
 
+  // STATE TO KEEP TRACK OF WHICH DROPDOWN MENU ITEM IS SELECTED; BY DEFAULT, ALL
+  const [galleryState, setGalleryState] = useState("All");
+
   return (
     <div id='content'>
     
       <Header/>
-      <NavMenu/>
-      <GalleryBody/>
+
+      {/* PASS DOWN DROPDOWN SELECTION AND FUNCTION TO UPDATE IT AS PROPS */}
+      <NavMenu galleryState={galleryState} setGalleryState={setGalleryState}/>
+
+      {/* PASS DOWN DROPDOWN SELECTION TO THE GALLERY FOR RE-RENDER */}
+      <GalleryBody galleryState={galleryState}/>
+
       <Footer/>
 
     </div>
