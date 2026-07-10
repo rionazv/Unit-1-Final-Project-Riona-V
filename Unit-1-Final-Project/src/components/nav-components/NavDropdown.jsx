c3505099608518f9981f6a8fe05249f0bae6610e
Riona <rionav02@gmail.com>
2026-07-10 12:55:31 -0500

    Added useState and useEffect logic to keep track of selection in preparation for filtering logic.

--- Unit-1-Final-Project/src/components/nav-components/NavDropdown.jsx
+++ Unit-1-Final-Project/src/components/nav-components/NavDropdown.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function NavDropdown() {
+
+    const [galleryState, setGalleryState] = useState("All");
     
+    function handleChange(event) {
+
+        const newGalleryState = event.target.value;
+        setGalleryState(newGalleryState);
+
+    }
+
+    useEffect(() => {
+        console.log(galleryState);
+    }, [galleryState]);
+
     return (
 
         <div id="nav-dropdown">
             
-            <select name="sticker-set" id="sticker-set">
+            <select name="sticker-set" id="sticker-set" onChange={handleChange}>
 
-                <option value="All" selected>All</option>
+                <option value="All">All</option>
                 <option value="From Finality, the Origin">From Finality, the Origin</option>
                 <option value="Soaring Good Fortune">Soaring Good Fortune</option>
                 <option value="Dance of Life and Death">Dance of Life and Death</option>
@@ -16,5 +31,5 @@ export default function NavDropdown() {
         </div>
 
     );
-
 }
+
