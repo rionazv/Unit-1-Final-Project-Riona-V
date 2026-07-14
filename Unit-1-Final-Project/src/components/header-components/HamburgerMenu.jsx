c82de54febdeda8b1c8faca374ba20ca773e91f5
Riona <rionav02@gmail.com>
2026-07-14 14:39:53 -0500

    Prepared new components and modified old ones to prepare for mobile responsive layout.

---
+++ Unit-1-Final-Project/src/components/header-components/HamburgerMenu.jsx
@@ -0,0 +1,11 @@
+export default function HamburgerMenu() {
+
+    return(
+
+        <div id="hamburger-menu-div">
+            burger
+        </div>
+
+    );
+
+}
--- Unit-1-Final-Project/src/components/header-components/Header..jsx
+++ Unit-1-Final-Project/src/components/header-components/Header..jsx
@@ -1,9 +1,18 @@
+import HamburgerMenu from "./HamburgerMenu";
+import PageNavigation from "./PageNavigation";
+
 export default function Header() {
     
     return (
 
         <header id="header-section">
+
+            <HamburgerMenu />
+
+            <PageNavigation />
+
             <h1>Hyperion Hoard</h1>
+
         </header>
 
     );
