b563cf614dd0614b1b8cb0b3f4fc6a8ec8a1c9c7
Riona <rionav02@gmail.com>
2026-07-15 14:35:12 -0500

    Populated home and about pages with basic content.

--- Unit-1-Final-Project/src/components/landing-page-components/AboutBody.jsx
+++ Unit-1-Final-Project/src/components/landing-page-components/AboutBody.jsx
@@ -4,7 +4,20 @@ export default function AboutBody() {
 
         <section id="about-body-section">
 
-            
+            <img className="big-display-valk" src="./src/assets/misc/Vita.png" alt="Valkyrie splash." />
+
+            <section className="landing-page-info">
+
+                <div className="info-blurb">
+                    <h2>About this project</h2>
+                    <p>Welcome to the hoard!</p>
+                </div>
+
+                <div className="info-blurb">
+                    <h3>What else?</h3>
+                </div>
+
+            </section>
 
         </section>
 
--- Unit-1-Final-Project/src/components/landing-page-components/HomeBody.jsx
+++ Unit-1-Final-Project/src/components/landing-page-components/HomeBody.jsx
@@ -4,7 +4,20 @@ export default function HomeBody() {
 
         <section id="home-body-section">
 
-            
+            <img className="big-display-valk" src="./src/assets/misc/Kiana.png" alt="Valkyrie splash." />
+
+            <section className="landing-page-info">
+
+                <div className="info-blurb">
+                    <h2>Captain on bridge!</h2>
+                    <p>Welcome to the hoard!</p>
+                </div>
+
+                <div className="info-blurb">
+                    <h3>Important Links</h3>
+                </div>
+
+            </section>
 
         </section>
 
