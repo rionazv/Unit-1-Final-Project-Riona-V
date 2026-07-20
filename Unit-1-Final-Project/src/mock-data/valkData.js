fc8130c1d80b3aaa84204adba99d7bf59233d261
Riona <rionav02@gmail.com>
2026-07-20 13:18:09 -0500

    Changing file structure for correct image display on deployment.

--- Unit-1-Final-Project/src/mock-data/valkData.js
+++ Unit-1-Final-Project/src/mock-data/valkData.js
@@ -6,207 +6,207 @@ export const valkData = [
     },
     {
         id: "Raiden Mei",
-        url: "./src/assets/valk-chibi-icons/mei.png",
+        url: "/valk-chibi-icons/mei.png",
         characters: "Mei"
     },
     {
         id: "Bronya Zaychik",
-        url: "./src/assets/valk-chibi-icons/bronya.png",
+        url: "/valk-chibi-icons/bronya.png",
         characters: "Bronya"
     },
     {
         id: "Murata Himeko",
-        url: "./src/assets/valk-chibi-icons/himeko.png",
+        url: "/valk-chibi-icons/himeko.png",
         characters: "Himeko"
     },
     {
         id: "Theresa Apocalypse",
-        url: "./src/assets/valk-chibi-icons/theresa.png",
+        url: "/valk-chibi-icons/theresa.png",
         characters: "Theresa"
     },
     {
         id: "Fu Hua",
-        url: "./src/assets/valk-chibi-icons/fuhua.png",
+        url: "/valk-chibi-icons/fuhua.png",
         characters: "Fu Hua"
     },
     {
         id: "Herrsher of Sentience",
-        url: "./src/assets/valk-chibi-icons/senti.png",
+        url: "/valk-chibi-icons/senti.png",
         characters: "Senti"
     },
     {
         id: "Rita Rossweisse",
-        url: "./src/assets/valk-chibi-icons/rita.png",
+        url: "/valk-chibi-icons/rita.png",
         characters: "Rita"
     },
     {
         id: "Yae Sakura",
-        url: "./src/assets/valk-chibi-icons/yae.png",
+        url: "/valk-chibi-icons/yae.png",
         characters: "Yae Sakura"
     },
     {
         id: "Kallen Kaslana",
-        url: "./src/assets/valk-chibi-icons/kallen.png",
+        url: "/valk-chibi-icons/kallen.png",
         characters: "Kallen"
     },
     {
         id: "Rozaliya Olenyeva",
-        url: "./src/assets/valk-chibi-icons/rozaliya.png",
+        url: "/valk-chibi-icons/rozaliya.png",
         characters: "Rozaliya"
     },
     {
         id: "Liliya Olenyeva",
-        url: "./src/assets/valk-chibi-icons/liliya.png",
+        url: "/valk-chibi-icons/liliya.png",
         characters: "Liliya"
     },
     {
         id: "Seele Vollerei",
-        url: "./src/assets/valk-chibi-icons/seele.png",
+        url: "/valk-chibi-icons/seele.png",
         characters: "Seele"
     },
     {
         id: "Dark Seele",
-        url: "./src/assets/valk-chibi-icons/veliona.png",
+        url: "/valk-chibi-icons/veliona.png",
         characters: "Veliona"
     },
     {
         id: "Biana Ataegina",
-        url: "./src/assets/valk-chibi-icons/durandal.png",
+        url: "/valk-chibi-icons/durandal.png",
         characters: "Durandal"
     },
     {
         id: "Fischl",
-        url: "./src/assets/valk-chibi-icons/fischl.png",
+        url: "/valk-chibi-icons/fischl.png",
         characters: "Fischl"
     },
     {
         id: "Elysia",
-        url: "./src/assets/valk-chibi-icons/elysia.png",
+        url: "/valk-chibi-icons/elysia.png",
         characters: "Elysia"
     },
     {
         id: "Mobius",
-        url: "./src/assets/valk-chibi-icons/mobius.png",
+        url: "/valk-chibi-icons/mobius.png",
         characters: "Mobius"
     },
     {
         id: "Natasha Cioara",
-        url: "./src/assets/valk-chibi-icons/raven.png",
+        url: "/valk-chibi-icons/raven.png",
         characters: "Raven"
     },
     {
         id: "Carole Pepper",
-        url: "./src/assets/valk-chibi-icons/carole.png",
+        url: "/valk-chibi-icons/carole.png",
         characters: "Carole"
     },
     {
         id: "Pardofelis",
-        url: "./src/assets/valk-chibi-icons/pardofelis.png",
+        url: "/valk-chibi-icons/pardofelis.png",
         characters: "Pardofelis"
     },
     {
         id: "Aponia",
-        url: "./src/assets/valk-chibi-icons/aponia.png",
+        url: "/valk-chibi-icons/aponia.png",
         characters: "Aponia"
     },
     {
         id: "Eden",
-        url: "./src/assets/valk-chibi-icons/eden.png",
+        url: "/valk-chibi-icons/eden.png",
         characters: "Eden"
     },
     {
         id: "Griseo",
-        url: "./src/assets/valk-chibi-icons/griseo.png",
+        url: "/valk-chibi-icons/griseo.png",
         characters: "Griseo"
     },
     {
         id: "Vill-V",
-        url: "./src/assets/valk-chibi-icons/villv.png",
+        url: "/valk-chibi-icons/villv.png",
         characters: "Vill-V"
     },
     {
         id: "Li Sushang",
-        url: "./src/assets/valk-chibi-icons/sushang.png",
+        url: "/valk-chibi-icons/sushang.png",
         characters: "Sushang"
     },
     {
         id: "Ai Hyperion",
-        url: "./src/assets/valk-chibi-icons/aichan.png",
+        url: "/valk-chibi-icons/aichan.png",
         characters: "Ai-Chan"
     },
     {
         id: "Susannah Manatt",
-        url: "./src/assets/valk-chibi-icons/susannah.png",
+        url: "/valk-chibi-icons/susannah.png",
         characters: "Susannah"
     },
     {
         id: "Misteln Schariac",
-        url: "./src/assets/valk-chibi-icons/misteln.png",
+        url: "/valk-chibi-icons/misteln.png",
         characters: "Misteln"
     },
     {
         id: "PROMETHEUS",
-        url: "./src/assets/valk-chibi-icons/prometheus.png",
+        url: "/valk-chibi-icons/prometheus.png",
         characters: "PROMETHEUS"
     },
     {
         id: "Shigure Kira",
-        url: "./src/assets/valk-chibi-icons/kira.png",
+        url: "/valk-chibi-icons/kira.png",
         characters: "Kira"
     },
     {
         id: "Sirin",
-        url: "./src/assets/valk-chibi-icons/sirin.png",
+        url: "/valk-chibi-icons/sirin.png",
         characters: "Sirin"
     },
     {
         id: "Senadina",
-        url: "./src/assets/valk-chibi-icons/senadina.png",
+        url: "/valk-chibi-icons/senadina.png",
         characters: "Senadina"
     },
     {
         id: "Coralie 6626 Planck",
-        url: "./src/assets/valk-chibi-icons/coralie.png",
+        url: "/valk-chibi-icons/coralie.png",
         characters: "Coralie"
     },
     {
         id: "Erdos Helia",
-        url: "./src/assets/valk-chibi-icons/helia.png",
+        url: "/valk-chibi-icons/helia.png",
         characters: "Helia"
     },
     {
         id: "Thelema Nutriscu",
-        url: "./src/assets/valk-chibi-icons/thelema.png",
+        url: "/valk-chibi-icons/thelema.png",
         characters: "Thelema"
     },
     {
         id: "Tsavorae",
-        url: "./src/assets/valk-chibi-icons/lantern.png",
+        url: "/valk-chibi-icons/lantern.png",
         characters: "Lantern"
     },
     {
         id: "Vita",
-        url: "./src/assets/valk-chibi-icons/vita.png",
+        url: "/valk-chibi-icons/vita.png",
         characters: "Vita"
     },
     {
         id: "Sparkle",
-        url: "./src/assets/valk-chibi-icons/sparkle.png",
+        url: "/valk-chibi-icons/sparkle.png",
         characters: "Sparkle"
     },
     {
         id: "Serapeum",
-        url: "./src/assets/valk-chibi-icons/serapeum.png",
+        url: "/valk-chibi-icons/serapeum.png",
         characters: "Serapeum"
     },
     {
         id: "Chenxue",
-        url: "./src/assets/valk-chibi-icons/chenxue.png",
+        url: "/valk-chibi-icons/chenxue.png",
         characters: "Chenxue"
     },
     {
         id: "Bailu Youyun",
-        url: "./src/assets/valk-chibi-icons/youyun.png",
+        url: "/valk-chibi-icons/youyun.png",
         characters: "Youyun"
     },
 ];
